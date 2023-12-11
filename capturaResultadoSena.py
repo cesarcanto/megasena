16562d0df276ac1f30dd72af941830ef1091511e
cesarcanto <cesarlcanto@gmail.com>
2023-12-11 20:59:16 -0300

    adicionado exibicao melhorada dos resultados com icones e msgs de avisos

--- capturaResultadoSena.py
+++ capturaResultadoSena.py
@@ -1,42 +1,33 @@
 import requests
+import sys
+
+# Mensagem indicando início da captura na API da Mega Sena
+print("✅ Iniciando captura na API da Mega Sena. Aguarde, carregando...")
 
 # API da Caixa para obter o resultado mais recente da Mega Sena
 url = 'https://apiloterias.com.br/app/v2/resultado?loteria=megasena'
 
-def obter_resultado_mega_sena():
-    response = requests.get(url)
-    if response.status_code == 200:
-        resultado = response.json()
-        # Imprime o resultado completo para entender sua estrutura
-        # print(resultado)
-        numeros_sorteio = resultado.get('dezenas', [])
-        return numeros_sorteio
-    else:
-        print("Falha ao obter os números sorteados")
-        return None
-
-def obter_data_mewga_sena():
-    response = requests.get(url)
-    if response.status_code == 200:
-        resultado = response.json()
-        # Imprime o resultado completo para entender sua estrutura
-        # print(resultado)
-        data_sorteio = resultado.get('data_concurso')
-        return data_sorteio
-    else:
-        print("Falha ao obter os números sorteados")
-        return None
+# Função para buscar os dados da API da Mega Sena
+def obter_dados_mega_sena():
+    try:
+        response = requests.get(url, timeout=40)  # Tempo limite de 10 segundos
+        response.raise_for_status()  # Levanta uma exceção se a resposta indicar erro
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("❌ Falha ao obter os números sorteados:", e)
+        sys.exit(1)  # Encerra o programa com código de erro 1
     
-# Salva o resultado obtido em uma variável
-resultado_mega_sena = obter_resultado_mega_sena()
 
-data_sorteio_mega_sena = obter_data_mewga_sena()
+# Obtém os dados da Mega Sena
+dados_mega_sena = obter_dados_mega_sena()
 
-# Imprimir a data do jogo e os números sorteados
-# if resultado_mega_sena:
-#     print("-----------------------------------------------------")
-#     print(f"A data do jogo Mega Sena é: {data_sorteio_mega_sena}")
-#     print("-----------------------------------------------------")
-#     print(f"Os números sorteados da Mega Sena foram: {', '.join(map(str, resultado_mega_sena))}")
-#     print("-----------------------------------------------------")
+# Se os dados foram obtidos com sucesso, prosseguir
+if dados_mega_sena:
+    numeros_sorteio = dados_mega_sena.get('dezenas', [])
+    data_sorteio = dados_mega_sena.get('data_concurso')
+    premiacao = dados_mega_sena.get('premiacao')
 
+    # Aqui você pode usar os dados obtidos da API da Mega Sena como necessário
+
+# Mensagem indicando finalização da captura na API da Mega Sena
+print("✅ Finalizada a captura na API da Mega Sena.")
--- jogoSena.py
+++ jogoSena.py
@@ -1,33 +1,71 @@
-from capturaResultadoSena import resultado_mega_sena, data_sorteio_mega_sena
-    
+from capturaResultadoSena import obter_dados_mega_sena
 
-def comparar_numeros(jogo_atual, numeros_jogo):
-    numeros_acertados = len(jogo_atual.intersection(numeros_jogo))
-    return numeros_acertados
+# Mensagem indicando que o programa está rodando
+print("✅ Programa para verificação de resultados da Mega Sena em andamento...")
 
-# Abrir o arquivo com os jogos da Mega Sena
-nome_arquivo = 'massa/jogos.txt'  # Nome do seu arquivo
-with open(nome_arquivo, 'r') as file:
-    jogos_da_mega_sena = file.readlines()
+# Obtém os dados da Mega Sena
+dados_mega_sena = obter_dados_mega_sena()
 
-# Processar os números dos jogos da Mega Sena
-for numeros in jogos_da_mega_sena:
-    numeros = numeros.strip().split()  # Separa por espaços por padrão
-    numeros_do_jogo = set(map(int, numeros))
-    acertos = comparar_numeros(set(resultado_mega_sena), numeros_do_jogo)
+# Verifica se os dados foram obtidos com sucesso
+if dados_mega_sena:
+    resultados = set(dados_mega_sena.get('dezenas', []))
+    data_sorteio = dados_mega_sena.get('data_concurso')
+    premiacao = dados_mega_sena.get('premiacao')
+
+    # Restante do seu código
+    nome_arquivo = 'massa/jogos.txt'
+
+    quantidade_apostas = 0
+    jogos_da_mega_sena = []
+
+    with open(nome_arquivo, 'r') as file:
+        linhas_arquivo = file.readlines()
+
+    # Listas para armazenar informações sobre os acertos
+    acertos_6 = []
+    acertos_5 = []
+    acertos_4 = []
+
+    for linha in linhas_arquivo:
+        quantidade_apostas += 1
+        numeros = linha.strip().split()  # Separa por espaços por padrão
+        numeros_do_jogo = set(map(int, numeros))
+        acertos = len(resultados.intersection(numeros_do_jogo))
+
+        if acertos == 6:
+            acertos_6.append(numeros)
+
+        elif acertos == 5:
+            acertos_5.append(numeros)
+
+        elif acertos == 4:
+            acertos_4.append(numeros)
+
+    # Exibir as informações consolidadas
+    print("🎉 Resultados da Mega Sena - 500 Apostas 🎉\n")
+    print("Oi pessoal,")
+    print("Atualizando sobre as apostas na Mega Sena: \n")
+    print(f"Data do Sorteio: {data_sorteio}\n")
+    print(f"🔢 Números Sorteados: {resultados}\n")
+    print("✅ Acertos:")
+
+    if acertos_6:
+        print("\nSENA - 6 números:")
+        for numeros in acertos_6:
+            print(f"{numeros}")
     
-    
-    # Destacar se houver mais de 4 acertos no mesmo jogo
-    if acertos >= 4:
-        print("-----------------------------------------------------")
-        print(f"A data do jogo Mega Sena é: {data_sorteio_mega_sena}")
-        print("-----------------------------------------------------")
-        print(f"No jogo {numeros}, você acertou {acertos} números.")
-        print("-----------------------------------------------------")
-    else:
-        print("-----------------------------------------------------")
-        print(f"A data do jogo Mega Sena é: {data_sorteio_mega_sena}")
-        print("-----------------------------------------------------")
-        print(f"Não houve nenhum jogo com 4 ou mais acertos!")
-        print("-----------------------------------------------------")
-        break
+    if acertos_5:
+        print("\nQUINA - 5 números:")
+        for numeros in acertos_5:
+            print(f"{numeros}")
+
+    if acertos_4:
+        print("\nQUADRA - 4 números:")
+        for numeros in acertos_4:
+            print(f"{numeros}")
+
+    print("\n🎉 Parabéns a todos! Comemoremos juntos! 🎉")
+    print(f"\n✅ Quantidade de jogos realizados: {quantidade_apostas}")
+
+else:
+    print("❌ Falha ao obter os números sorteados da Mega Sena")
