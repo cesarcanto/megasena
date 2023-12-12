cffbaae045556f22adad39c95f3ce234a43f4a46
cesarcanto <cesarlcanto@gmail.com>
2023-12-11 22:19:32 -0300

    adicionado exibicao melhorada dos resultados com icones e msgs de avisos

--- jogoSena.py
+++ jogoSena.py
@@ -1,62 +1,26 @@
 from capturaResultadoSena import obter_dados_mega_sena
 
-# Mensagem indicando que o programa está rodando
-print("✅ Programa para verificação de resultados da Mega Sena em andamento...")
+def padronizar_numeros(numeros):
+    return set(map(int, numeros))
 
-# Obtém os dados da Mega Sena
-dados_mega_sena = obter_dados_mega_sena()
+def validar_numeros(numeros):
+    return all(num.isdigit() for num in numeros)
 
-# Verifica se os dados foram obtidos com sucesso
-if dados_mega_sena:
-    resultados = set(dados_mega_sena.get('dezenas', []))
-    data_sorteio = dados_mega_sena.get('data_concurso')
-    premiacao = dados_mega_sena.get('premiacao')
-
-    # Restante do seu código
-    nome_arquivo = 'massa/jogos.txt'
-
-    quantidade_apostas = 0
-    jogos_da_mega_sena = []
-
-    with open(nome_arquivo, 'r') as file:
-        linhas_arquivo = file.readlines()
-
-    # Lista para armazenar informações sobre os acertos
-    acertos = {
-        6: [],
-        5: [],
-        4: [],
-        3: []
-    }
-
-    for linha in linhas_arquivo:
-        quantidade_apostas += 1
-        numeros = linha.strip().split()  # Separa por espaços por padrão
-        numeros_do_jogo = set(map(int, numeros))
-        qtd_acertos = len(resultados.intersection(numeros_do_jogo))
-
-        if qtd_acertos in [3, 4, 5, 6]:
-            acertos[qtd_acertos].append(numeros_do_jogo)
-
-    # Exibir as informações consolidadas
-    print(f"⚠️ Atualizando sobre as apostas na Mega Sena:\n")
-    print(f"⚠️ Quantidade de apostas realizadas: {quantidade_apostas}\n")
-    print(f"📅 Data do Sorteio: {data_sorteio}\n")  # Emoji de calendário na data
-    print(f"🔢 Números Sorteados: {resultados}\n")
-    print(f"✅ Resultados: ")
-
-    acertos_totais = sum([len(acertos[i]) for i in range(3, 7)])
+def exibir_informacoes_acertos(acertos):
+    acertos_totais = sum([len(acertos[i]) for i in range(2, 7)])
     
     for acerto, jogos in acertos.items():
         if jogos:
             if acerto == 6:
-                print(f"\nSENA - 6 números: {acerto}")
+                print(f"\nSENA - 6: {acerto}")
             elif acerto == 5:
-                print(f"\nQUINA - 5 números: {acerto}")
+                print(f"\nQUINA - 5: {acerto}")
             elif acerto == 4:
-                print(f"\nQUADRA - 4 números: {acerto}")
+                print(f"\nQUADRA - 4: {acerto}")
             elif acerto == 3:
-                print(f"\nTerno - 3 números: {acerto}")
+                print(f"\nTerno - 3: {acerto}")
+            elif acerto == 2:
+                print(f"\nDuque - 2: {acerto}")
             
             for jogo in jogos:
                 print(f"Números: {jogo}")
@@ -68,3 +32,56 @@ if dados_mega_sena:
         print("\n😞 Que pena, não acertamos nada dessa vez!")
     else:
         print("\n🎉 Parabéns a todos! Comemoremos juntos! 🎉")
+
+def processar_resultados_acertos(resultados, linhas_arquivo):
+    acertos = {
+        6: [],
+        5: [],
+        4: [],
+        3: [],
+        2: []
+    }
+
+    for linha in linhas_arquivo:
+        numeros = linha.strip().split()  # Separa por espaços por padrão
+
+        if validar_numeros(numeros):
+            numeros_do_jogo = set(map(int, numeros))
+            qtd_acertos = len(resultados.intersection(numeros_do_jogo))
+
+            if qtd_acertos in range(1, 7):
+                acertos[qtd_acertos].append(numeros_do_jogo)
+
+    return acertos
+
+def obter_e_processar_resultados():
+    dados_mega_sena = obter_dados_mega_sena()
+
+    if dados_mega_sena:
+        resultados = padronizar_numeros(dados_mega_sena.get('dezenas', []))
+        resultados = set(resultados)
+        data_sorteio = dados_mega_sena.get('data_concurso')
+        premiacao = dados_mega_sena.get('premiacao')
+
+        nome_arquivo = 'massa/jogoteste.txt'
+
+        quantidade_apostas = 0
+
+        with open(nome_arquivo, 'r') as file:
+            linhas_arquivo = file.readlines()
+
+        acertos = processar_resultados_acertos(resultados, linhas_arquivo)
+
+        print(f"⚠️ Atualizando sobre as apostas na Mega Sena:\n")
+        print(f"⚠️ Quantidade de apostas realizadas: {len(linhas_arquivo)}\n")
+        print(f"📅 Data do Sorteio: {data_sorteio}\n")  
+        print(f"🔢 Números Sorteados: {resultados}\n")
+        print(f"✅ Resultados: ")
+
+        exibir_informacoes_acertos(acertos)
+    else:
+        print("\n😞 Não foi possível obter os resultados desta vez!")
+
+# Mensagem indicando que o programa está rodando
+print("✅ Programa para verificação de resultados da Mega Sena em andamento...")
+obter_e_processar_resultados()
